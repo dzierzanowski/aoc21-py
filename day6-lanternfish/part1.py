83b233d456823b5289aa8448bd6ccfb5005da1e8
Kamil Dzierzanowski <Kamil.Dzierzanowski@vimn.com>
2021-12-06 11:50:01 +0100

    Add day6

---
+++ day6-lanternfish/part1.py
@@ -0,0 +1,18 @@
+#!python3
+
+with open('input.txt') as file:
+    fish = [ int(i) for i in file.read().split(',') ]
+
+days = 80
+
+states = [0 for _ in range(0, 9)]
+
+for timer in fish:
+    states[timer] += 1
+
+for _ in range(0, days):
+    spawning_count = states.pop(0)
+    states.append(spawning_count)
+    states[6] += spawning_count
+
+print(f'Result: {sum(states)}')
