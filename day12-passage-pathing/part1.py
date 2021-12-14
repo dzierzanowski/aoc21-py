04dab0c1c0040a3ff012a7085076e71e4f63f003
Kamil Dzierzanowski <Kamil.Dzierzanowski@vimn.com>
2021-12-14 12:41:07 +0100

    Add day12

---
+++ day12-passage-pathing/part1.py
@@ -0,0 +1,40 @@
+#!python3
+
+with open('input.txt') as file:
+    links = [
+        line.split('-')
+        for line in file.read().split('\n')
+    ]
+
+nodes = {}
+
+for link in links:
+    start, end = link
+    if start not in nodes:
+        nodes[start] = {
+            'small': start.islower(),
+            'neighbors': []
+        }
+    if end not in nodes:
+        nodes[end] = {
+            'small': end.islower(),
+            'neighbors': []
+        }
+    nodes[start]['neighbors'].append(end)
+    nodes[end]['neighbors'].append(start)
+
+paths = []
+
+candidates = [
+    [ 'start' ]
+]
+while candidates:
+    curr = candidates.pop()
+    last = curr[-1]
+    for neigh in nodes[last]['neighbors']:
+        if neigh == 'end':
+            paths.append(curr.copy() + [ 'end' ])
+        elif neigh not in curr or not nodes[neigh]['small']:
+            candidates.append(curr.copy() + [ neigh ])
+
+print(f'Path count: {len(paths)}')
